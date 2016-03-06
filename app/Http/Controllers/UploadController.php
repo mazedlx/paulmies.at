7f9089ae84b172246c5da827544b3a3f4cc0bbe9
Christian Leo <mazedlx@gmail.com>
2016-03-06 17:43:07 +0100

    fixed upload index

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -22,7 +22,7 @@ class UploadController extends Controller
      */
     public function index()
     {
-        $uploads = Upload::orderBy('sort', 'asc')->get()->paginate(10);
+        $uploads = Upload::orderBy('sort', 'asc')->paginate(10);
 
         return view('uploads.index')
             ->with('uploads', $uploads);

2502d15ef671f5cf13d77d76f5d135a33eeb57a5
Christian Leo <mazedlx@gmail.com>
2016-03-06 17:11:21 +0100

    changed css href

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>paulmies.at - Administration</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body id="app-layout">
 <div class="container">

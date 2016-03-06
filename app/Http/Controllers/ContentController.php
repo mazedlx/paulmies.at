10459081fb1111fe8b9c1fc9b63349ed1d57e7d4
Christian Leo <mazedlx@gmail.com>
2016-03-06 18:07:45 +0100

    Inquery View

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -117,7 +117,8 @@ class ContentController extends Controller
 
     public function inquiry(Request $request)
     {
-        Mail::send('mails.inquiry', ['message' => $request->message], function ($message) {
+        $data[] = $request->message;
+        Mail::send('mails.inquiry', $data, function ($message) {
             $message->from('office@paulmies.at', 'Website-Anfrage');
             $message->to(Configuration::config('email')->get()->first()->value, Configuration::config('name')->get()->first()->value);
             $message->subject('Website-Anfrage');

8695a6324a197c70b14dade5037a54378670032e
Christian Leo <mazedlx@gmail.com>
2016-03-11 09:51:59 +0100

    new logo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#home"><img src="images/logo-pm.png" alt="logo"></a>
+                <a class="navbar-brand" href="#home"><img src="images/logo-pm.png" alt="logo"></a> {{ $configs['logotext'] }}
             </div>
             <div class="collapse navbar-collapse navbar-right">
                 <ul class="nav navbar-nav">
